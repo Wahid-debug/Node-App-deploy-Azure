bcd939e386177c2ce593eb634262144ceb3959aa
@wahid-debug <wahid.wonderpillars@gmail.com>
2022-03-02 11:59:30 +0530

    this commit

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 3000
 app.get("/", (req,res)=> {
     res.send("This is server1")
 })
+app.get("/about", (req,res)=> {
+    res.send("This is about")
+})
 app.listen(port, ()=> {
     console.log(`Server Running on Port ${port} `)
 })
